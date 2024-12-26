b9a91a2dba28290ba718fcb2f4ca695ebd3cac36
E0SelmY4V <yabo1887415157@qq.com>
2024-12-26 11:44:12 +0800

    ref: 能使用的版本

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use num_traits::int::PrimInt;
 use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     fmt::Binary,
     hash::Hash,
 };
@@ -11,31 +10,19 @@ pub mod judge;
 
 pub use expr_dis::display_expr;
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum StepMethod<T> {
+pub enum Step<T> {
     Not(T),
     And(T, T),
     Or(T, T),
     Exist,
     Magic,
 }
-#[derive(Clone, Copy, Debug)]
-pub struct Step<S, T>(S, StepMethod<T>);
-impl<S: PrimInt, T> PartialEq for Step<S, T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-}
-impl<S: PrimInt, T> Eq for Step<S, T> {}
-impl<S: PrimInt, T> PartialOrd for Step<S, T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl<S: PrimInt, T> Ord for Step<S, T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.0.cmp(&other.0)
-    }
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Route<T> {
+    Not,
+    And(T),
+    Or(T),
 }
 
 pub enum Expr<T> {
@@ -46,138 +33,213 @@ pub enum Expr<T> {
     Magic,
 }
 
-pub struct Maker<S, T> {
+pub struct Maker<T> {
     pub factors: HashSet<T>,
-    makings: HashSet<T>,
-    pub step_map: HashMap<T, Step<S, T>>,
+    pub target: T,
+    targets: VecDeque<T>,
+    pub step_map: HashMap<T, Step<T>>,
+    pub routes: HashMap<T, HashSet<Route<T>>>,
 }
 
-impl<S: PrimInt, T: PrimInt + Hash + Binary> Maker<S, T> {
-    pub fn new(factors: HashSet<T>) -> Maker<S, T> {
-        let step_map = HashMap::from_iter(
-            factors
-                .iter()
-                .map(|&factor| (factor, Step(S::zero(), StepMethod::Exist))),
-        );
+impl<T: PrimInt + Hash + Binary> Maker<T> {
+    pub fn new(factors: HashSet<T>, target: T) -> Maker<T> {
+        let step_map: HashMap<T, Step<T>> =
+            HashMap::from_iter(factors.iter().map(|&factor| (factor, Step::Exist)));
+        let routes: HashMap<T, HashSet<Route<T>>> =
+            HashMap::from_iter(factors.iter().map(|&factor| (factor, HashSet::new())));
         Maker {
             factors,
-            makings: HashSet::new(),
+            target,
+            targets: VecDeque::from([target]),
             step_map,
+            routes,
         }
     }
-    pub fn make(&mut self, target: T) -> Step<S, T> {
-        if self.makings.contains(&target) {
-            return Step(S::max_value(), StepMethod::Magic);
-        }
-        if let Some(&step) = self.step_map.get(&target) {
-            // println!("exist {:0>8}", format!("{:b}", target));
-            return step;
-        }
-        // println!("making {:0>8}", format!("{:b}", target));
-        self.makings.insert(target);
-        let step = self
-            .test_and(target)
-            .min(self.test_not(target))
-            .min(self.test_or(target));
-        self.makings.remove(&target);
-        if step.1 != StepMethod::Magic {
-            self.step_map.insert(target, step);
-            println!("success {:0>8}", format!("{:b}", target));
+    fn deal_route(&mut self, route: Route<T>, target: T, fined: bool) -> bool {
+        if fined {
+            match route {
+                Route::Not => {
+                    if !self.step_map.contains_key(&!target) {
+                        self.step_map.insert(!target, Step::Not(target));
+                        if self.fine(!target) {
+                            return true;
+                        }
+                    }
+                }
+                Route::And(n) => {
+                    if self.step_map.contains_key(&n) && !self.step_map.contains_key(&(n & target))
+                    {
+                        self.step_map.insert(n & target, Step::And(n, target));
+                        if self.fine(n & target) {
+                            return true;
+                        }
+                    }
+                }
+                Route::Or(n) => {
+                    if self.step_map.contains_key(&n) && !self.step_map.contains_key(&(n | target))
+                    {
+                        self.step_map.insert(n | target, Step::Or(n, target));
+                        if self.fine(n | target) {
+                            return true;
+                        }
+                    }
+                }
+            }
         } else {
-            // println!("failed {:0>8}", format!("{:b}", target));
+            self.routes
+                .entry(target)
+                .or_insert(HashSet::new())
+                .insert(route);
         }
-        step
+        false
     }
-    pub fn test_and(&mut self, target: T) -> Step<S, T> {
+    fn fine(&mut self, target: T) -> bool {
+        // println!("fine {:0>8}", format!("{:b}", target));
+        let routes = self.routes.remove(&target);
+        if target == self.target {
+            return true;
+        }
+        for route in routes.unwrap() {
+            if self.deal_route(route, target, true) {
+                return true;
+            }
+        }
+        false
+    }
+    pub fn make(&mut self) {
+        let mut e: usize = 400;
+        while let Some(target) = self.targets.pop_front() {
+            if self.factors.remove(&target) {
+                // println!("success {:0>8}", format!("{:b}", target));
+                if self.fine(target) {
+                    return;
+                }
+            } else if !self.step_map.contains_key(&target) {
+                // println!("making {:0>8}", format!("{:b}", target));
+                if self.test_and(target) || self.test_or(target) || self.test_not(target) {
+                    return;
+                }
+            }
+            e -= 1;
+            if e == 0 {
+                break;
+            }
+        }
+    }
+    pub fn test_and(&mut self, target: T) -> bool {
         // println!("andin {:0>8}", format!("{:b}", target));
-        let mut min_step = Step(S::max_value(), StepMethod::Magic);
         let mut check = target;
         let mut counter = T::one();
         let mut pos_ori: Vec<T> = vec![];
+        let mut size = T::zero();
         while counter != T::zero() {
             if check >> 1 << 1 == check {
                 pos_ori.push(counter);
+                size = (size << 1) + T::one();
             }
             counter = counter << 1;
             check = check >> 1;
         }
         let mut pos = vec![];
-        for n in pos_ori {
-            let step = self.make(target | n);
-            if step.1 != StepMethod::Magic {
-                pos.push(n | target);
+        loop {
+            counter = T::one();
+            check = T::zero();
+            for no in 0..pos_ori.len() {
+                if size | counter == size {
+                    check = check | pos_ori[no];
+                }
+                counter = counter << 1;
             }
+            pos.push(check);
+            if size == T::zero() {
+                break;
+            }
+            size = size - T::one();
         }
         for i in 0..pos.len() {
+            if !self.step_map.contains_key(&(target | pos[i])) {
+                self.targets.push_back(target | pos[i]);
+            }
             for j in 0..i {
                 let a = target | pos[i];
                 let b = target | pos[j];
                 if a & b == target {
-                    let a_step = self.make(a);
-                    let b_step = self.make(b);
-                    min_step = min_step.min(Step(a_step.0 + b_step.0, StepMethod::And(a, b)));
+                    if self.deal_route(Route::And(b), a, self.step_map.contains_key(&a)) {
+                        return true;
+                    }
+                    if self.deal_route(Route::And(a), b, self.step_map.contains_key(&b)) {
+                        return true;
+                    }
                 }
             }
         }
+        false
         // println!("andout {:0>8}", format!("{:b}", target));
-        min_step
     }
-    pub fn test_or(&mut self, target: T) -> Step<S, T> {
+    pub fn test_or(&mut self, target: T) -> bool {
         // println!("orin {:0>8}", format!("{:b}", target));
-        let mut min_step = Step(S::max_value(), StepMethod::Magic);
         let mut check = target;
         let mut counter = T::one();
         let mut pos_ori: Vec<T> = vec![];
+        let mut size = T::zero();
         while counter != T::zero() {
             if check >> 1 << 1 != check {
                 pos_ori.push(counter);
+                size = (size << 1) + T::one();
             }
             counter = counter << 1;
             check = check >> 1;
         }
         let mut pos = vec![];
-        for n in pos_ori {
-            let step = self.make(target - n);
-            if step.1 != StepMethod::Magic {
-                pos.push(target - n);
+        loop {
+            counter = T::one();
+            check = T::zero();
+            for no in 0..pos_ori.len() {
+                if size | counter == size {
+                    check = check | pos_ori[no];
+                }
+                counter = counter << 1;
             }
+            pos.push(check);
+            if size == T::zero() {
+                break;
+            }
+            size = size - T::one();
         }
         for i in 0..pos.len() {
+            if !self.step_map.contains_key(&(target - pos[i])) {
+                self.targets.push_back(target - pos[i]);
+            }
             for j in 0..i {
                 let a = target - pos[i];
                 let b = target - pos[j];
                 if a | b == target {
-                    let a_step = self.make(a);
-                    let b_step = self.make(b);
-                    min_step = min_step.min(Step(a_step.0 + b_step.0, StepMethod::Or(a, b)));
+                    if self.deal_route(Route::Or(b), a, self.step_map.contains_key(&a)) {
+                        return true;
+                    }
+                    if self.deal_route(Route::Or(a), b, self.step_map.contains_key(&b)) {
+                        return true;
+                    }
                 }
             }
         }
+        false
         // println!("orout {:0>8}", format!("{:b}", target));
-        min_step
     }
-    fn test_not(&mut self, target: T) -> Step<S, T> {
-        let step = self.make(!target);
-        if step.1 == StepMethod::Magic {
-            return step;
+    fn test_not(&mut self, target: T) -> bool {
+        let fined = self.step_map.contains_key(&target);
+        if !fined {
+            self.targets.push_back(!target);
         }
-        Step(step.0 + S::one(), StepMethod::Not(!target))
+        self.deal_route(Route::Not, target, fined)
     }
     pub fn get_expr(&self, target: T) -> Expr<T> {
-        match *self
-            .step_map
-            .get(&target)
-            .unwrap_or(&Step(S::max_value(), StepMethod::Magic))
-        {
-            Step(_, StepMethod::And(a, b)) => {
-                Expr::And(Box::new(self.get_expr(a)), Box::new(self.get_expr(b)))
-            }
-            Step(_, StepMethod::Or(a, b)) => {
-                Expr::Or(Box::new(self.get_expr(a)), Box::new(self.get_expr(b)))
-            }
-            Step(_, StepMethod::Not(n)) => Expr::Not(Box::new(self.get_expr(n))),
-            Step(_, StepMethod::Exist) => Expr::Exist(target),
-            Step(_, StepMethod::Magic) => Expr::Magic,
+        match *self.step_map.get(&target).unwrap_or(&Step::Magic) {
+            Step::And(a, b) => Expr::And(Box::new(self.get_expr(a)), Box::new(self.get_expr(b))),
+            Step::Or(a, b) => Expr::Or(Box::new(self.get_expr(a)), Box::new(self.get_expr(b))),
+            Step::Not(n) => Expr::Not(Box::new(self.get_expr(n))),
+            Step::Exist => Expr::Exist(target),
+            Step::Magic => Expr::Magic,
         }
     }
     pub fn check(&self, target: T) -> bool {
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,22 @@ use std::collections::HashSet;
 use make_up_binary::{display_expr, Maker};
 
 fn main() {
-	let mut maker: Maker<u8, u8> = Maker::new(HashSet::from([
+	let target = 0b00010000;
+	let mut maker: Maker<u8> = Maker::new(HashSet::from([
 		0b01010101,
 		0b00110011,
 		0b00001111,
-	]));
-	let target = 0b00000101;
+	]), target);
 	if !maker.check(target) {
 		for num in maker.check_detail(target) {
 			println!("{:b}", num);
 		}
 		return;
 	}
+	maker.make();
 	// maker.test_or(target);
 	// return;
 	let expr = maker.get_expr(target);
-	println!("{:#?}", maker.make(target));
+	// println!("{:#?}", maker.make(target));
 	println!("{}", display_expr(&expr));
 }
